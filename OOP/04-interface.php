19bc5280540725526ed5cc2e5efd228cddde33c4
rtraselbd <rtraselbd72735@gmail.com>
2023-11-22 11:48:13 +0600

    OOP

--- OOP/04-interface.php
+++ OOP/04-interface.php
@@ -7,11 +7,12 @@ interface GatewayInterface
     function ipn();
 }
 
-class bKash implements GatewayInterface
+
+class Gateway implements GatewayInterface
 {
     public function createCharge( $data )
     {
-        var_dump( $data );
+        return $data;
     }
 
     public function paymentSuccess()
@@ -30,40 +31,6 @@ class bKash implements GatewayInterface
     }
 }
 
-class Main
-{
-    public $name;
-
-    public function __construct( $name )
-    {
-        $this->name = $name;
-    }
-}
-
-class Gateway extends Main implements GatewayInterface
-{
-    public function createCharge( $data )
-    {
-        $class = $this->name;
-        $init = new $class;
-        return $init->createCharge( $data );
-    }
-
-    public function paymentSuccess()
-    {
-
-    }
-
-    public function paymentCancel()
-    {
-
-    }
-
-    public function ipn()
-    {
-
-    }
-}
-
-$gateway = new Gateway( 'bKash' );
-$gateway->createCharge( ['name' => 'Rasel'] );
+$gateway = new Gateway();
+$result = $gateway->createCharge( ['name' => 'Rasel'] );
+print_r($result);
--- OOP/05-static-method.php
+++ OOP/05-static-method.php
@@ -3,10 +3,11 @@
 abstract class UPApi{
     protected static $api_key;
     protected static $api_url;
+
+    abstract public function createCharge();
 }
 
 class UddoktaPay extends UPApi{
-    //private static $instance;
 
     public function __construct($apiKey, $apiUrl){
         parent::$api_key = $apiKey;
@@ -16,18 +17,8 @@ class UddoktaPay extends UPApi{
     public function createCharge(){
         echo parent::$api_key;
     }
-
-    // public static function init(){
-    //     if(self::$instance == null){
-    //         self::$instance = new UddoktaPay;
-    //     }
-    //     return self::$instance;
-    // }
 }
 
 $up = new UddoktaPay('Test API', 'Test URL');
 
 echo $up->createCharge();
-
-
-// $up = UddoktaPay::init();
